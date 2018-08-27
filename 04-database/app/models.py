576de9b3069410bdaab924013820f5161be230d9
Kyrell Dixon <kyrell.brown@gmail.com>
2018-08-27 18:02:01 -0500

    Added post model

--- 04-database/app/models.py
+++ 04-database/app/models.py
@@ -7,4 +7,13 @@ class User(db.Model):
   password_hash = db.Column(db.String(128))
 
   def __repr__(self):
-    return f'<User {self.username}>'
+    return f'<User {self.username}>'t
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  body = db.Column(db.String(140))
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  def __repr__(self):
+    return '<Post {}>'.format(self.body)
