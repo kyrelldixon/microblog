02f28320f6f8cb77c45bc84b90d1e752aa1766c9
Kyrell Dixon <kyrell.brown@gmail.com>
2018-08-28 09:15:33 -0500

    Added shell context to ease database operations

--- 04-database/microblog.py
+++ 04-database/microblog.py
@@ -1,1 +1,14 @@
 from app import app
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+  """
+  Registers items in the context of the `flask shell` environment
+  Items returned will not have to be manually imported
+  """
+  return {
+    'db': db,
+    'User': User,
+    'Post': Post
+  }
