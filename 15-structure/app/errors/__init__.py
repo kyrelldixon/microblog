1980da1fda6aa6272e77f467710292294fc58a27
Kyrell Dixon <kyrell.brown@gmail.com>
2018-08-28 22:43:47 -0500

    added template folder to blueprint

--- 15-structure/app/errors/__init__.py
+++ 15-structure/app/errors/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-bp = Blueprint('errors', __name__)
+bp = Blueprint('errors', __name__, template_folder='templates')
 
 # !IMPORTANT location here to prevent circular import
 from app.errors import views
