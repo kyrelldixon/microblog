c04e67f2ebc18e89c14724a24bd40b8e237a73c6
Kyrell Dixon <kyrell.brown@gmail.com>
2018-08-28 22:43:13 -0500

    Added auth blueprints

--- 15-structure/app/__init__.py
+++ 15-structure/app/__init__.py
@@ -16,10 +16,13 @@ migrate = Migrate(app, db)
 # Custom Middleware
 login = LoginManager(app)
 bootstrap = Bootstrap(app)
-login.login_view = 'login'
+login.login_view = 'auth.login'
 
 # Blueprints
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp)
 
 from app import routes, models
