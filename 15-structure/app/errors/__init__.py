c9fd04e379a160d1d1a619e83b43a555c16d7475
Kyrell Dixon <kyrell.brown@gmail.com>
2018-08-29 19:11:07 -0500

    Fixed errorsgs

--- 15-structure/app/errors/__init__.py
+++ 15-structure/app/errors/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 bp = Blueprint('errors', __name__, template_folder='templates')
 
 # !IMPORTANT location here to prevent circular import
-from app.errors import views
+from app.errors import handlers
